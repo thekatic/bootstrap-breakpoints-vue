ad679ee50c73f1be48950ce38192f109d4b87bbb
Vlad <vladimir@gearsaid.com>
2020-03-26 01:04:09 +0100

    Add new breakpoint xxl

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const breakpoints = {
   sm: 576,
   md: 768,
   lg: 992,
-  xl: 1200
-}
+  xl: 1200,
+  xxl: 1440
+};
 
 const edge = 0.2;
 
-const delay = 250;// delay between calls
+const delay = 250; // delay between calls
 let throttled = false; // are we currently throttled?
 
 const plugin = {
-	install: (Vue) => {
-
+  install: Vue => {
     const bootstrap = {
       width: window.innerWidth
-    }
+    };
 
-    window.addEventListener('resize', () => {
+    window.addEventListener("resize", () => {
       // only run if we're not throttled
       if (!throttled) {
         // actual callback action
-        bootstrap.width = window.innerWidth
+        bootstrap.width = window.innerWidth;
 
         // we're throttled!
         throttled = true;
@@ -30,61 +30,78 @@ const plugin = {
         setTimeout(function() {
           throttled = false;
         }, delay);
-      }  
+      }
     });
 
-		Vue.mixin({
-			beforeCreate() {
-        Vue.util.defineReactive(this, '_bootstrap', bootstrap);
-			}
-		});
+    Vue.mixin({
+      beforeCreate() {
+        Vue.util.defineReactive(this, "_bootstrap", bootstrap);
+      }
+    });
 
-		Object.defineProperty(Vue.prototype, '$bootstrap', {
-			get: function() {
-				return this.$root._bootstrap;
-			}
-    }); 
+    Object.defineProperty(Vue.prototype, "$bootstrap", {
+      get: function() {
+        return this.$root._bootstrap;
+      }
+    });
 
     Vue.prototype.$mediaBreakpointUp = function(key) {
       if (key) {
-        return this.$root._bootstrap.width >= breakpoints[key]
+        return this.$root._bootstrap.width >= breakpoints[key];
       }
-      return null
-		};
+      return null;
+    };
 
     Vue.prototype.$mediaBreakpointDown = function(key) {
       if (key) {
-        return this.$root._bootstrap.width < (breakpoints[key] - edge)
+        return this.$root._bootstrap.width < breakpoints[key] - edge;
       }
-      return null
+      return null;
     };
-    
+
     Vue.prototype.$mediaBreakpointBetween = function(start, end) {
-      if (start, end) {
-        return this.$root._bootstrap.width >= breakpoints[start] && this.$root._bootstrap.width < breakpoints[end]
+      if ((start, end)) {
+        return (
+          this.$root._bootstrap.width >= breakpoints[start] &&
+          this.$root._bootstrap.width < breakpoints[end]
+        );
       }
-      return null
-		};
+      return null;
+    };
 
     Vue.prototype.$mediaBreakpointOnly = function(key) {
-      if (key === 'xs') {
-        return this.$root._bootstrap.width < (breakpoints.sm - edge)
-      } else if (key === 'sm') {
-        return this.$root._bootstrap.width >= breakpoints.sm && this.$root._bootstrap.width < (breakpoints.md - edge)
-      } else if (key === 'md') {
-        return this.$root._bootstrap.width >= breakpoints.md && this.$root._bootstrap.width < (breakpoints.lg - edge)
-      } else if (key === 'lg') {
-        return this.$root._bootstrap.width >= breakpoints.lg && this.$root._bootstrap.width < (breakpoints.xl - edge)
-      } else if (key === 'xl') {
-        return this.$root._bootstrap.width >= breakpoints.xl
+      if (key === "xs") {
+        return this.$root._bootstrap.width < breakpoints.sm - edge;
+      } else if (key === "sm") {
+        return (
+          this.$root._bootstrap.width >= breakpoints.sm &&
+          this.$root._bootstrap.width < breakpoints.md - edge
+        );
+      } else if (key === "md") {
+        return (
+          this.$root._bootstrap.width >= breakpoints.md &&
+          this.$root._bootstrap.width < breakpoints.lg - edge
+        );
+      } else if (key === "lg") {
+        return (
+          this.$root._bootstrap.width >= breakpoints.lg &&
+          this.$root._bootstrap.width < breakpoints.xl - edge
+        );
+      } else if (key === "xl") {
+        return (
+          this.$root._bootstrap.width >= breakpoints.xl &&
+          this.$root._bootstrap.width < breakpoints.xxl - edge
+        );
+      } else if (key === "xxl") {
+        return this.$root._bootstrap.width >= breakpoints.xxl;
       }
-      return null
+      return null;
     };
-	}
+  }
 };
 
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(plugin)
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(plugin);
 }
 
 export default plugin;
